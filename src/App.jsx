d0f25c4aba69df7e23a4b2778f4d53d8b8a1fcd5
rebecaverasa <rebecaverasa@gmail.com>
2023-06-24 17:04:37 -0300

    Carousel deleted. NavBar layout is ready.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar.jsx';
-import Carousel from './components/Carousel/Carousel.jsx';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <br></br>
-      <Carousel />
     </div>
   );
 }
