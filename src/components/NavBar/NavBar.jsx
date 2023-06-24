088857f7543ef4bfaaec811b3ca64d771ab87f4a
rebecaverasa <rebecaverasa@gmail.com>
2023-06-24 16:49:23 -0300

    NavBar was styled with Bryaxis's brand layout. It was added a basket icon for the shopping list.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, Container, NavDropdown, Form, Button } from 'react-bootstrap';
-import styles from  './NavBar.modules.css';
+import './NavBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
 class NavBar extends Component {
     render() {
         return (
             <div>
-                <Navbar expand="lg" className="bg-navbar">
+                <Navbar expand="lg" className="navbar-custom">
                     <Container fluid>
-                        <Navbar.Brand href="#">Ebookstore</Navbar.Brand>
+                        <Navbar.Brand href="#" className="navbar-brand">BRYAXIS</Navbar.Brand>
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll">
                             <Nav
@@ -16,31 +18,27 @@ class NavBar extends Component {
                                 style={{ maxHeight: '100px' }}
                                 navbarScroll
                             >
-                                <Nav.Link href="#action1">Home</Nav.Link>
-                                <Nav.Link href="#action2">Link</Nav.Link>
-                                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action4">
-                                        Another action
-                                    </NavDropdown.Item>
+                                <Nav.Link href="#action1" className="navbar-home">Home</Nav.Link>
+                                <Nav.Link href="#action2" className="navbar-link1">Link1</Nav.Link>
+                                <NavDropdown title="Link2" id="navbarScrollingDropdown" className='navbar-dropdown'>
+                                    <NavDropdown.Item href="#action3" className="dropdown-item">Action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action4" className="dropdown-item">Another action</NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action5">
-                                        Something else here
-                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action5" className="dropdown-item">Something else here</NavDropdown.Item>
                                 </NavDropdown>
-                                <Nav.Link href="#" disabled>
-                                    Link
-                                </Nav.Link>
                             </Nav>
                             <Form className="d-flex">
                                 <Form.Control
                                     type="search"
                                     placeholder="Search"
-                                    className="me-2"
+                                    className="search-bar"
                                     aria-label="Search"
                                 />
-                                <Button variant="outline-success">Search</Button>
+                                <Button variant="outline-success" className="search-button">Search</Button>
                             </Form>
+                            <Nav.Link href="#" className="navbar-cart-icon">
+                                <FontAwesomeIcon icon={faShoppingBasket} className="icon-basket" />
+                            </Nav.Link>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
